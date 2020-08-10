048325d650dd63b45edcf312eb9c18ec7a6e67a1
H <H@et.com>
2020-08-10 15:26:50 +0800

    在家开发其他的权退功能

---
+++ signIn.java
@@ -0,0 +1,14 @@
+/*
+* @Author: Marte
+* @Date:   2020-08-10 14:57:07
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-08-10 15:24:38
+*/
+
+public class java {
+    private String name;
+    private String sign_time;
+    public static void main(String[] args) {
+
+    }
+}
