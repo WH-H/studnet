83b33a144ccea7f40754ee3ca9db414c725f4d5c
et2002 <et2002@et.com>
2020-08-10 15:22:00 +0800

    在公司开发了部分功能

---
+++ signIn.java
@@ -0,0 +1,15 @@
+/*
+* @Author: Marte
+* @Date:   2020-08-10 14:49:50
+* @Last Modified by:   Marte
+* @Last Modified time: 2020-08-10 15:20:57
+*/
+
+public class java {
+    private int id;
+    private String name;
+
+    public static void main(String[] args) {
+
+    }
+}
